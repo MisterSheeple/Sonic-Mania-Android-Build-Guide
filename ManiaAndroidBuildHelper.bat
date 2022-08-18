9f4c2c47ae1a837991ccb63ddd62bd1b39d96e06
MisterSheeple <sheepbusiness12@gmail.com>
2022-08-17 23:32:26 -0400

    BuildHelper v1.4
    - Made things more efficient by cloning the Mania repo recursively, eliminating the need to manually clone three other repos.

--- ManiaAndroidBuildHelper.bat
+++ ManiaAndroidBuildHelper.bat
@@ -1,6 +1,6 @@
 @echo off
 echo ------------------------------------------------------
-echo MisterSheeple's Sonic Mania Android Build Helper v1.3
+echo MisterSheeple's Sonic Mania Android Build Helper v1.4
 echo ------------------------------------------------------
 echo.
 echo                                                  WARNING:
@@ -21,33 +21,29 @@ goto clone
 
 :clone
 cd C:\
-git clone https://github.com/Rubberduckycooly/Sonic-Mania-Decompilation.git C:\Sonic-Mania-Decompilation
-git clone https://github.com/Rubberduckycooly/RSDKv5-Decompilation.git C:\RSDKv5-Decompilation
+git clone https://github.com/Rubberduckycooly/Sonic-Mania-Decompilation.git C:\Sonic-Mania-Decompilation --recursive
 git clone https://github.com/Rubberduckycooly/RSDKv5-Example-Mods.git C:\RSDKv5-Example-Mods
 git clone https://github.com/Rubberduckycooly/GameAPI.git C:\GameAPI
-cd C:\RSDKv5-Decompilation\dependencies\all
-git clone https://github.com/leethomason/tinyxml2.git C:\RSDKv5-Decompilation\dependencies\all\tinyxml2
-git clone https://github.com/nothings/stb.git C:\RSDKv5-Decompilation\dependencies\all\stb_vorbis
 echo Cloning done.
 cls
 goto dependencies
 
 :dependencies
-cd C:\RSDKv5-Decompilation\dependencies\android
+cd C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android
 curl -L http://downloads.xiph.org/releases/theora/libtheora-1.1.1.zip --output libtheora.zip
 curl -L http://downloads.xiph.org/releases/ogg/libogg-1.3.5.zip --output libogg.zip
-powershell -command "Expand-Archive -LiteralPath 'C:\RSDKv5-Decompilation\dependencies\android\libtheora.zip' -DestinationPath 'C:\RSDKv5-Decompilation\dependencies\android\'"
-powershell -command "Expand-Archive -LiteralPath 'C:\RSDKv5-Decompilation\dependencies\android\libogg.zip' -DestinationPath 'C:\RSDKv5-Decompilation\dependencies\android\'"
+powershell -command "Expand-Archive -LiteralPath 'C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android\libtheora.zip' -DestinationPath 'C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android\'"
+powershell -command "Expand-Archive -LiteralPath 'C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android\libogg.zip' -DestinationPath 'C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android\'"
 rename libtheora-1.1.1 libtheora
-for /f %%a IN ('dir "C:\RSDKv5-Decompilation\dependencies\android\libogg-1.3.5" /b') do move "C:\RSDKv5-Decompilation\dependencies\android\libogg-1.3.5\%%a" "C:\RSDKv5-Decompilation\dependencies\android\libogg\"
-move C:\RSDKv5-Decompilation\dependencies\android\libogg-1.3.5\include\Makefile.am C:\RSDKv5-Decompilation\dependencies\android\libogg\include\Makefile.am
-move C:\RSDKv5-Decompilation\dependencies\android\libogg-1.3.5\include\Makefile.in C:\RSDKv5-Decompilation\dependencies\android\libogg\include\Makefile.in
-move C:\RSDKv5-Decompilation\dependencies\android\libogg-1.3.5\include\ogg\config_types.h.in C:\RSDKv5-Decompilation\dependencies\android\libogg\include\ogg\config_types.h.in
-move C:\RSDKv5-Decompilation\dependencies\android\libogg-1.3.5\include\ogg\Makefile.am C:\RSDKv5-Decompilation\dependencies\android\libogg\include\ogg\Makefile.am
-move C:\RSDKv5-Decompilation\dependencies\android\libogg-1.3.5\include\ogg\Makefile.in C:\RSDKv5-Decompilation\dependencies\android\libogg\include\ogg\Makefile.in
-move C:\RSDKv5-Decompilation\dependencies\android\libogg-1.3.5\include\ogg\ogg.h C:\RSDKv5-Decompilation\dependencies\android\libogg\include\ogg\ogg.h
-move C:\RSDKv5-Decompilation\dependencies\android\libogg-1.3.5\include\ogg\os_types.h C:\RSDKv5-Decompilation\dependencies\android\libogg\include\ogg\os_types.h
-rmdir C:\RSDKv5-Decompilation\dependencies\android\libogg-1.3.5\ /s /q
+for /f %%a IN ('dir "C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android\libogg-1.3.5" /b') do move "C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android\libogg-1.3.5\%%a" "C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android\libogg\"
+move C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android\libogg-1.3.5\include\Makefile.am C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android\libogg\include\Makefile.am
+move C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android\libogg-1.3.5\include\Makefile.in C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android\libogg\include\Makefile.in
+move C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android\libogg-1.3.5\include\ogg\config_types.h.in C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android\libogg\include\ogg\config_types.h.in
+move C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android\libogg-1.3.5\include\ogg\Makefile.am C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android\libogg\include\ogg\Makefile.am
+move C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android\libogg-1.3.5\include\ogg\Makefile.in C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android\libogg\include\ogg\Makefile.in
+move C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android\libogg-1.3.5\include\ogg\ogg.h C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android\libogg\include\ogg\ogg.h
+move C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android\libogg-1.3.5\include\ogg\os_types.h C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android\libogg\include\ogg\os_types.h
+rmdir C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\dependencies\android\libogg-1.3.5\ /s /q
 del libtheora.zip libogg.zip
 echo Dependencies done.
 cls
@@ -56,22 +52,22 @@ goto touchsetup
 :touchsetup
 cd C:\RSDKv5-Example-Mods\ManiaTouchControls
 if /I %touch%==n (goto ultrawidesetup) else (mklink /D "GameAPI" C:\GameAPI)
-cd C:\RSDKv5-Decompilation\android\app\jni
+cd C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\android\app\jni
 mklink /D "MTC" C:\RSDKv5-Example-Mods\ManiaTouchControls
 goto ultrawidesetup
 
 :ultrawidesetup
 cd C:\RSDKv5-Example-Mods\UltrawideMania
 if /I %ultrawide%==n (goto mainsymlinksetup) else (mklink /D "GameAPI" C:\GameAPI)
-cd C:\RSDKv5-Decompilation\android\app\jni
+cd C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\android\app\jni
 mklink /D "UM" C:\RSDKv5-Example-Mods\UltrawideMania
 goto mainsymlinksetup
 
 :mainsymlinksetup
-cd C:\RSDKv5-Decompilation\android\app\jni
+cd C:\Sonic-Mania-Decompilation\dependencies\RSDKv5\android\app\jni
 mklink /D "GameAPI" C:\GameAPI
 mklink /D "Game" C:\Sonic-Mania-Decompilation
-mklink /D "RSDKv5" C:\RSDKv5-Decompilation
+mklink /D "RSDKv5" C:\Sonic-Mania-Decompilation\dependencies\RSDKv5
 echo Symlink setup done.
 goto finish
 
